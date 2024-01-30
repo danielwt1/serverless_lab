aefd56bc163d30e5a6fa7cb1dedb3f3ae8100ebf
Daniel Ramos <dramosg@ucentral.edu.co>
2024-01-30 08:25:16 -0500

    First commit

---
+++ poc_lambda/TicketsRegister.js
@@ -0,0 +1,7 @@
+'use strict';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+export const handler = async (event,context) => {
+  
+  
+};
---
+++ poc_lambda/createtableTechnical.js
@@ -0,0 +1,8 @@
+'use strict';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+export const handler = async (event,context) => {
+    console.log(event);
+  
+  
+};
